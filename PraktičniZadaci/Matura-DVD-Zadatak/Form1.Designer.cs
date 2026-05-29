a1168448aec67c86b98d50a81ff4da379ff72646
0matt <maxydesing@gmail.com>
2026-05-29 15:52:48 +0200

    Praktični Zadaci
    DVD Zadatak - A13

---
+++ PraktičniZadaci/Matura-DVD-Zadatak/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Matura_DVD_Zadatak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripSeparator2,
+            this.toolStripButton3,
+            this.toolStripSeparator4,
+            this.toolStripButton4,
+            this.toolStripSeparator3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(70, 450);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(67, 59);
+            this.toolStripButton1.Text = "Izmeni";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(67, 6);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(67, 59);
+            this.toolStripButton2.Text = "Analiza";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(67, 6);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(67, 59);
+            this.toolStripButton3.Text = "O aplikaciji";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(67, 6);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(67, 59);
+            this.toolStripButton4.Text = "Izlaz";
+            this.toolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(67, 6);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(320, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(178, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(320, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Šifra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "E-mail";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(114, 210);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(674, 204);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(122, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Šifra";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(175, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Naziv producentske kuće";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(406, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "E-mail adresa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Korekcija podataka - producentska kuca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PraktičniZadaci/Matura-DVD-Zadatak/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+// Sql
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+/*
+ ______   ______     __   __   ______        ______   ______     ______     __    __     ______    
+/\  == \ /\  == \   /\ \ / /  /\  __ \      /\  ___\ /\  __ \   /\  == \   /\ "-./  \   /\  __ \   
+\ \  _-/ \ \  __<   \ \ \'/   \ \  __ \     \ \  __\ \ \ \/\ \  \ \  __<   \ \ \-./\ \  \ \  __ \  
+ \ \_\    \ \_\ \_\  \ \__|    \ \_\ \_\     \ \_\    \ \_____\  \ \_\ \_\  \ \_\ \ \_\  \ \_\ \_\ 
+  \/_/     \/_/ /_/   \/_/      \/_/\/_/      \/_/     \/_____/   \/_/ /_/   \/_/  \/_/   \/_/\/_/   
+*/
+
+
+
+namespace Matura_DVD_Zadatak {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        // Sql konekcioni string
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DVD;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e) {
+            con.Open(); // OTVARANJE KONEKCIJE
+
+            string upit = "select ProducentID, Ime, Email from Producent"; // Sql upit za selekciju i ovo kasnije trebamo da prikazemo u listBox-u
+
+            SqlCommand cmd = new SqlCommand(upit, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while(dr.Read()) { 
+                string s = dr[0].ToString() + "\t" + dr[1].ToString() + "\t" + dr[2].ToString() + "\t";
+
+                listBox1.Items.Add(s);
+            }
+
+            con.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            textBox1.Text = listBox1.SelectedItem.ToString().Split('\t')[0];
+            textBox2.Text = listBox1.SelectedItem.ToString().Split('\t')[1];
+            textBox3.Text = listBox1.SelectedItem.ToString().Split('\t')[2];
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e) {
+            con.Open();
+
+            string upit1 = "update Producent set Ime=@ime, Email=@mejl where ProducentID=@id";
+
+            SqlCommand cmd1 = new SqlCommand(upit1, con);
+
+            listBox1.Items.Clear();
+            cmd1.Parameters.AddWithValue("@ime", textBox2.Text);
+            cmd1.Parameters.AddWithValue("@mejl", textBox3.Text);
+            cmd1.Parameters.AddWithValue("@id", textBox1.Text);
+            cmd1.ExecuteNonQuery();
+
+            string upit2 = "select ProducentID, Ime, Email from Producent"; // Sql upit za selekciju i ovo kasnije trebamo da prikazemo u listBox-u
+
+            listBox1.Items.Clear();
+
+            SqlCommand cmd2 = new SqlCommand(upit2, con);
+            SqlDataReader dr1 = cmd2.ExecuteReader();
+
+            while (dr1.Read())
+            {
+                string s1 = dr1[0].ToString() + "\t" + dr1[1].ToString() + "\t" + dr1[2].ToString() + "\t";
+
+                listBox1.Items.Add(s1);
+            }
+
+            MessageBox.Show("Uspesna izmena !");
+
+            con.Close();
+        }
+
+        /*
+          ___  ___ _   _  ___   _     ___ ___  ___ __  __   _   
+         |   \| _ \ | | |/ __| /_\   | __/ _ \| _ \  \/  | /_\  
+         | |) |   / |_| | (_ |/ _ \  | _| (_) |   / |\/| |/ _ \ 
+         |___/|_|_\\___/ \___/_/ \_\ |_| \___/|_|_\_|  |_/_/ \_\
+                                                        
+            -> NAPRAVIS DRUGU FORMU I OVO JE KOD ZA NJU
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                string upit="SELECT a.Ime, COUNT(b.FilmID) AS \"Broj filmova\" FROM Producent a, Producirao b WHERE a.ProducentID = b.ProducentID GROUP BY a.Ime"; -> sql query
+
+                chart1.Series.Clear();
+                chart1.Series.Add("Filmovi");
+
+                SqlCommand kom = new SqlCommand(upit,kon);
+
+                con.Open();	
+
+                SqlDataAdapter da = new SqlDataAdapter(kom);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                con.Close();
+
+                int i = 0;
+                foreach (DataRow item in dt.Rows) 
+                {
+                    chart1.Series[0].Points.AddXY(item[0].ToString(), item[1]);
+                    i++;
+                }
+             } 
+         */
+
+    }
+}
